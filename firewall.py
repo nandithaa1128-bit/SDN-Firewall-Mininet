70985532a71371dbe6213e9e620bd1d4dfe81c41
Nanditha <nandithaa1128@gmail.com>
2026-04-24 13:53:07 +0530

    Added firewall.py code

---
+++ firewall.py
@@ -0,0 +1,33 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+
+class Firewall(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        parser = datapath.ofproto_parser
+
+        # BLOCK rule → h1 to h2
+        match = parser.OFPMatch(ipv4_src="10.0.0.1", ipv4_dst="10.0.0.2")
+        actions = []  # empty = DROP
+
+        self.add_flow(datapath, 100, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
